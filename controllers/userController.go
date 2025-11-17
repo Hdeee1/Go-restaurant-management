f56fb1f44d63bbb3ccc78cc6a0f881db57a45b1a
Hadyu Fiqran Hakim <hadyuhakim@gmail.com>
2025-11-17 11:26:20 +0700

    setup: controllers

---
+++ controllers/userController.go
@@ -0,0 +1,33 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func HashPassword(password string) string {
+	
+}
+
+func VerifyPassword(password string, providedPassword string) (bool, string) {} 
+
+func GetUsers() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func GetUser() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func SignUp() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func Login() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
