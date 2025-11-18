98be9c4b28cce5fa8ff77480b5e5bcb65da94667
Hadyu Fiqran Hakim <hadyuhakim@gmail.com>
2025-11-18 14:21:33 +0700

    create functions for hash and verify password

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,12 +1,27 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func HashPassword(password string) string {
-	
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(bytes)
 }
 
-func VerifyPassword(password string, providedPassword string) (bool, string) {} 
+func VerifyPassword(password string, providedPassword string) (bool, string) {
+	err := bcrypt.CompareHashAndPassword([]byte(HashPassword(password)), []byte(providedPassword))
+	if err != nil {
+		return false, "Wrong password"
+	}
+
+	return true, ""
+}
 
 func GetUsers() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
