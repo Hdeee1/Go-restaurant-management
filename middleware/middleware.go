c4f3fdd9134d30021befc28b87e9d5465be787d5
Hadyu Fiqran Hakim <hadyuhakim@gmail.com>
2025-11-20 13:34:40 +0700

    create auth middleware function

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,5 +1,30 @@
 package middleware
 
-func Authentication() {
-	
+import (
+	"net/http"
+	"strings"
+
+	"github.com/Hdeee1/go-restaurant-management/helpers"
+	"github.com/gin-gonic/gin"
+)
+
+func Authentication() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		token := ctx.GetHeader("Authorization")
+		if token == "" {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "No authorization header"})
+			return 
+		}
+
+		tokenString := strings.TrimPrefix(token, "Bearer")
+
+		claims, err := helpers.ValidateToken(tokenString)
+		if err != nil {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
+			return 
+		}
+
+		ctx.Set("email", claims.Email)
+		ctx.Set("user_id", claims.User_id)
+	}
 }
