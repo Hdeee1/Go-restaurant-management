52d92755ee0b4b859b4131bd8601ae47072ef090
Hadyu Fiqran Hakim <hadyuhakim@gmail.com>
2025-11-21 15:06:24 +0700

    bug in menu model

--- models/menuModel.go
+++ models/menuModel.go
@@ -9,7 +9,7 @@ import (
 type Menu struct {
 	gorm.Model
 	Name		string	`json:"name" validate:"required"`
-	Category	string	`json:"" validate:"required"`
+	Category	string	`json:"category" validate:"required"`
 	Start_date	*time.Time	`json:"start_date"`
 	End_date	*time.Time	`json:"end_date"`
 	Menu_id		string	`json:"food_id" validate:"required"`
