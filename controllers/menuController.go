8ad8617bda836a8a8e3f2919a96b2666b8d73465
Hadyu Fiqran Hakim <hadyuhakim@gmail.com>
2025-11-21 14:39:49 +0700

    create get menus, stuck create menu

--- controllers/menuController.go
+++ controllers/menuController.go
@@ -1,10 +1,27 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/Hdeee1/go-restaurant-management/database"
+	"github.com/Hdeee1/go-restaurant-management/models"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
 
 func GetMenus() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		var menus []models.Menu
 
+		err := database.DB.Find(&menus).Error
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return 
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"menus": menus,
+		})
 	}
 }
 
@@ -16,7 +33,16 @@ func GetMenu() gin.HandlerFunc {
 
 func CreateMenu() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		
+		var menu models.Menu
+
+		menu.Menu_id = uuid.New().String()
+
+		if err := ctx.BindJSON(&menu); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return 
+		}
+
+
 	}
 }
 
