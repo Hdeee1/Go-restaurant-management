a28c25f60bc39b10ef460a60b6f3f5ef80b86ff1
Hadyu Fiqran Hakim <hadyuhakim@gmail.com>
2025-11-24 09:12:26 +0700

    create function for create table

--- controllers/tableController.go
+++ controllers/tableController.go
@@ -1,6 +1,13 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	
+	"github.com/Hdeee1/go-restaurant-management/database"
+	"github.com/Hdeee1/go-restaurant-management/models"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
 
 func GetTables() gin.HandlerFunc {
 	return  func(ctx *gin.Context) {
@@ -16,7 +23,24 @@ func GetTable() gin.HandlerFunc {
 
 func CreateTable() gin.HandlerFunc {
 	return  func(ctx *gin.Context) {
-		
+		var table models.Table
+
+		table.Table_id = uuid.New().String()
+
+		if err := ctx.BindJSON(&table); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if err := database.DB.Create(&table).Error; err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		ctx.JSON(http.StatusCreated, gin.H{
+			"message": "table created",
+			"table_id": table.Table_id,
+		})
 	}
 }
 
