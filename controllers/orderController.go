e0ec672f944ea9060698a789433244c22c34a9a4
Hadyu Fiqran Hakim <hadyuhakim@gmail.com>
2025-11-24 09:44:54 +0700

    create function for create order

--- controllers/orderController.go
+++ controllers/orderController.go
@@ -1,6 +1,13 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	
+	"github.com/Hdeee1/go-restaurant-management/database"
+	"github.com/Hdeee1/go-restaurant-management/models"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
 
 func GetOrders() gin.HandlerFunc {
 	return  func(ctx *gin.Context) {
@@ -16,7 +23,24 @@ func GetOrder() gin.HandlerFunc {
 
 func CreateOrder() gin.HandlerFunc {
 	return  func(ctx *gin.Context) {
-		
+		var order models.Order
+
+		order.Order_id = uuid.New().String()
+
+		if err := ctx.BindJSON(&order); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if err := database.DB.Create(&order).Error; err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		ctx.JSON(http.StatusCreated, gin.H{
+			"message": "order created",
+			"order_id": order.Order_id,
+		})
 	}
 }
 
