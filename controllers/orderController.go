3fc6903e07c27523b3737554c72f7145f7016c1d
Hadyu Fiqran Hakim <hadyuhakim@gmail.com>
2025-11-24 09:46:17 +0700

    feat: implement `UpdateOrder` function to update an existing order by ID.

--- controllers/orderController.go
+++ controllers/orderController.go
@@ -64,6 +64,29 @@ func CreateOrder() gin.HandlerFunc {
 
 func UpdateOrder() gin.HandlerFunc {
 	return  func(ctx *gin.Context) {
-		
+		order_id := ctx.Param("order_id")
+
+		var order models.Order
+
+		if err := database.DB.Where("order_id = ?", order_id).First(&order).Error; err != nil {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": "order_id not found"})
+			return
+		}
+
+		var updateData models.Order
+		if err := ctx.BindJSON(&updateData); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if err := database.DB.Model(&order).Updates(updateData).Error; err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "order updated",
+			"order_id": order.Order_id,
+		})
 	}
 }
